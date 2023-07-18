2dd5457b222d90a672c4b5a324a0e61932b33233
Kevin Liu <kevinliu@Kevins-MBP.lan>
2023-07-17 18:23:38 -0700

    add await for bcrypt.compare in change-password

--- routes/password.js
+++ routes/password.js
@@ -20,7 +20,7 @@ router.put("/change-password", async (req, res) => {
     const authId = user.rows[0].authorization_id;
     const userRole = user.rows[0].role;
     const dbOldPw = user.rows[0].password;
-    const pwMatches = bcrypt.compare(oldPassword, dbOldPw);
+    const pwMatches = await bcrypt.compare(oldPassword, dbOldPw);
     console.warn("pwMatches = ", pwMatches);
 
     if (!pwMatches) return res.status(400).send('Invalid email or password.');
