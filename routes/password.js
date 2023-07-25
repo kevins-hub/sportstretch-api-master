899868f3d192f153f957113980786d2d879e7c51
Kevin Liu <kevinliu@Kevins-MBP.lan>
2023-07-24 22:15:04 -0700

    Item 12: Forgot password api (minus email service)

--- routes/password.js
+++ routes/password.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const config = require('config');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
@@ -12,6 +13,11 @@ const pool = new Pool({
     }
 }
 );
+
+const generateToken = () => {
+    // creates 5 digit token
+    return Math.random().toString(7).slice(2);
+}
 
 router.put("/change-password", async (req, res) => {
     const { email, newPassword, oldPassword } = req.body;
@@ -65,6 +71,29 @@ router.put("/reset-password", async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const newHashed = await bcrypt.hash(newPassword, salt);
     const updatePw = await pool.query("UPDATE tb_authorization SET password = $1 WHERE authorization_id = $2", [newHashed, authId]);
+    return res.status(200).json({
+        status: "success"
+    })
+});
+
+router.put("/forgot-password", async (req, res) => {
+
+    const { email } = req.body;
+
+    let user = await pool.query("SELECT * FROM tb_authorization WHERE email = $1", [email]);
+    if (user.rows.length === 0) return res.status(400).send("An account with this email does not exist. Please try again.");
+    const authId = user.rows[0].authorization_id;
+    const tokenValidDuration  = 5;
+    let expiration = new Date();
+    expiration = expiration.setMinutes(expiration.getMinutes() + tokenValidDuration);
+    const resetToken = generateToken();
+
+    // set reset token to user's entry in db
+    await pool.query("UPDATE tb_authorization SET pw_reset_token = $1 WHERE authorization_id = $2", [resetToken, authId]);
+    await pool.query("UPDATE tb_authorization SET pw_reset_expiration = $1 WHERE authorization_id = $2", [expiration, authId]);
+
+    // send token via email / text
+
     return res.status(200).json({
         status: "success"
     })
