ec3697e145710db14f1e25de77ebf47d11b8360e
Kevin Liu <kevinliu@Kevins-MBP.lan>
2023-11-25 16:38:11 -0800

    add int conversion for auth id in contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -12,25 +12,24 @@ const pool = new Pool({
 }
 );
 
-
 // edit contact info
 router.put("/edit-contact", async (req, res) => {
         const { authId, email, phone, addressL1, addressL2, city, state, zipcode } = req.body;
     
-        let user = await pool.query("SELECT * FROM tb_authorization WHERE authorization_id = $1", [authId]);
+        let user = await pool.query("SELECT * FROM tb_authorization WHERE authorization_id = $1", [parseInt(authId)]);
         const userRole = user.rows[0].role;
 
     
         if (userRole === 'athlete') {
-            const emailUpdate = await pool.query("UPDATE tb_authorization SET email = $1 WHERE authorization_id = $2", [email, authId]);
-            const athlete = await pool.query("UPDATE tb_athlete SET mobile = $1 WHERE fk_authorization_id = $2", [phone, authId]);
+            const emailUpdate = await pool.query("UPDATE tb_authorization SET email = $1 WHERE authorization_id = $2", [email, parseInt(authId)]);
+            const athlete = await pool.query("UPDATE tb_athlete SET mobile = $1 WHERE fk_authorization_id = $2", [phone, parseInt(authId)]);
             return res.status(200).json({
                 status: "success"
             })
         }
     
         if (userRole === 'therapist') {
-            const emailUpdate = await pool.query("UPDATE tb_authorization SET email = $1 WHERE authorization_id = $2", [email, authId]);
+            const emailUpdate = await pool.query("UPDATE tb_authorization SET email = $1 WHERE authorization_id = $2", [email, parseInt(authId)]);
             const therapist = await pool.query("UPDATE tb_therapist SET mobile = $1, apartment_no = $2, street = $3, city = $4, state = $5, zipcode = $6 WHERE fk_authorization_id = $7", [phone, addressL2, addressL1, city, state, zipcode, authId]);
             return res.status(200).json({
                 status: "success"
