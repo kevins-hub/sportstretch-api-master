690acb7d2b936d37cb5bd74dce3d7cbc776904f3
Kevin Liu <kevinliu@Kevins-MBP.lan>
2024-04-09 22:27:36 -0700

    add stripe register account endpoint

--- routes/payment.js
+++ routes/payment.js
@@ -2,25 +2,50 @@ const express = require("express");
 // const stripe = require("stripe")(
 //   process.env.STRIPE_SECRET
 // );
-const stripe = require("stripe")(
-  process.env.STRIPE_SECRET_TEST
-);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
 const router = express.Router();
 
 const calculateOrderAmount = (body) => {
-    const total = body.amount * 100;
-    return total;
-  };
+  const total = body.amount * 100;
+  return total;
+};
 
 router.post("/create-payment-intent", async (req, res) => {
   const body = req.body;
+  const totalAmount = calculateOrderAmount(body);
+  const platformFee = totalAmount * 0.1;
+  const stripeAccountId = body.stripeAccountId;
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: calculateOrderAmount(body),
+      amount: totalAmount,
       currency: "usd",
+      appication_fee_amount: platformFee,
+      transfer_data: {
+        destination: stripeAccountId,
+      }
     });
     res.send({
       clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+route.post("/register-stripe-account", async (req, res) => {
+  const body = req.body;
+  try {
+    const account = await stripe.accounts.create({
+      type: "express",
+      country: 'US',
+      email: body.email,
+      capabilities: {
+        card_payments: { requested: true },
+        transfers: { requested: true },
+      },
+    }); 
+    res.send({
+      account: account,
     });
   } catch (err) {
     console.log(err.message);
