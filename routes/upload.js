c62f3af88c5acf47549933bfac5955ec9a7d1e89
Kevin Liu <kevinliu@Kevins-MBP-2.lan>
2024-07-07 23:52:22 -0700

    add error handling for debug for upload endpoint

--- routes/upload.js
+++ routes/upload.js
@@ -15,40 +15,55 @@ const pool = new Pool({
 });
 
 // Configure AWS SDK
-AWS.config.update({
+
+try {
+  AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
   });
-  
-  const s3 = new AWS.S3();
-  console.warn("s3 = ", s3);
-  const bucketName = process.env.S3_BUCKET_NAME;
+  console.log("AWS SDK configured successfully.");
+} catch (error) {
+  console.error("Error configuring AWS SDK: ", error);
+}
+
+const s3 = new AWS.S3();
+console.warn("s3 = ", s3);
+const bucketName = process.env.S3_BUCKET_NAME;
 
 // Set up multer and multer-s3 to handle file uploads
 const upload = multer({
-    storage: multerS3({
-      s3,
-      bucket: bucketName,
-      acl: 'public-read',
-      key: function (req, file, cb) {
-        cb(null, `profile-pictures/${Date.now().toString()}-${file.originalname}`);
-      },
-    }),
-  });
-
-router.post("/profile-picture/:id", upload.single('file'), auth, async (req, res) => {
-    if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-      }
-      const imageUrl = req.file.location;
-
-      // Save imageUrl to your database in tb_authorization associated with the user (not shown)
-      const imageUpload = await pool.query(
-        "UPDATE tb_authorization SET profile_picture_url = $1 WHERE authorization_id = $2", [imageUrl, req.params.id]
+  storage: multerS3({
+    s3,
+    bucket: bucketName,
+    acl: "public-read",
+    key: function (req, file, cb) {
+      cb(
+        null,
+        `profile-pictures/${Date.now().toString()}-${file.originalname}`
       );
+    },
+  }),
+});
 
-      res.status(201).json({ imageUrl });
-    });
+router.post(
+  "/profile-picture/:id",
+  upload.single("file"),
+  auth,
+  async (req, res) => {
+    if (!req.file) {
+      return res.status(400).send("No file uploaded.");
+    }
+    const imageUrl = req.file.location;
+
+    // Save imageUrl to your database in tb_authorization associated with the user (not shown)
+    const imageUpload = await pool.query(
+      "UPDATE tb_authorization SET profile_picture_url = $1 WHERE authorization_id = $2",
+      [imageUrl, req.params.id]
+    );
+
+    res.status(201).json({ imageUrl });
+  }
+);
 
 module.exports = router;
