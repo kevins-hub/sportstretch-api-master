3b83ba8b40d841534afa48b2cf583cf3d7e49a78
Kevin Liu <kevinliu@Kevins-MBP-2.lan>
2024-07-07 23:30:35 -0700

    add aws import

--- routes/upload.js
+++ routes/upload.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const config = require("config");
 const auth = require("../middleware/auth");
+const AWS = require("aws-sdk");
 
 const Pool = require("pg").Pool;
 const pool = new Pool({
