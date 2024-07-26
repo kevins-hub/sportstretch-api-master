9a7c4f04b4feec97e0f96f8ac3c55f9a24de27d8
Kevin Liu <kevinliu@Kevins-MBP-2.lan>
2024-07-26 16:16:58 -0700

    update cron time

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const getAthleteTherapistContactInfo = async (therapist_id, athlete_id) => {
 }
 
 // cron job to run at midnight and send reminder emails to all therapists and athletes with appointments the next day
-schedule.scheduleJob("18 * * * *", async () => {
+schedule.scheduleJob("25 * * * *", async () => {
   const today = new Date();
   today.setDate(today.getDate());
   const todayString = today.toISOString().split("T")[0];
@@ -112,7 +112,7 @@ schedule.scheduleJob("18 * * * *", async () => {
 });
 
 // TODO: schedule job to run 30 minutes after midnight to charge athletes for their appointments
-schedule.scheduleJob("18 * * * *", async () => {
+schedule.scheduleJob("25 * * * *", async () => {
   // charge payment intent
   try {
     console.warn("charging payment intents");
