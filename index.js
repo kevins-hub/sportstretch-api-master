97d16a84b32bf082b5176923959c20daf6694a6b
Kevin Liu <kevinliu@Kevins-MBP-2.lan>
2024-07-27 00:37:42 -0700

    update capture method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cron = require("node-cron")
+const cron = require("node-cron");
 const config = require("config");
 const therapists = require("./routes/therapists");
 const bookings = require("./routes/bookings");
@@ -14,9 +14,9 @@ const contact = require("./routes/contact");
 const payment = require("./routes/payment");
 const report = require("./routes/report");
 const emailService = require("./utilities/email.js");
-const upload = require("./routes/upload.js")
-const profilePicture = require("./routes/profilePicture.js")
-const schedule = require("node-schedule")
+const upload = require("./routes/upload.js");
+const profilePicture = require("./routes/profilePicture.js");
+const schedule = require("node-schedule");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
 
 const Pool = require("pg").Pool;
@@ -39,14 +39,14 @@ app.use("/notifications", notifications);
 app.use("/contact", contact);
 app.use("/payment", payment);
 app.use("/report", report);
-app.use("/upload", upload)
+app.use("/upload", upload);
 app.use("/profilePicture", profilePicture);
 
 app.get("/", (req, res) => {
   res.send("Sportstretch server is running!");
 });
 
-const getTodaysBookings = async ()  => {
+const getTodaysBookings = async () => {
   // get all bookings from tb_bookings where booking_date = today (YYYY-MM-DD)
   const today = new Date();
   today.setDate(today.getDate());
@@ -56,7 +56,7 @@ const getTodaysBookings = async ()  => {
     [todayString]
   );
   return result.rows;
-}
+};
 
 const updateBookingStatus = async (bookingId, status) => {
   const result = await pool.query(
@@ -64,7 +64,7 @@ const updateBookingStatus = async (bookingId, status) => {
     [status, bookingId]
   );
   return result.rowCount === 1;
-}
+};
 
 const getAthleteTherapistContactInfo = async (therapist_id, athlete_id) => {
   // query tb_authorization, tb_therapist, and tb_athlete to get therapist auth id, therapist first name, therapist email, athlete auth id, athlete first name, and athlete email
@@ -80,8 +80,7 @@ const getAthleteTherapistContactInfo = async (therapist_id, athlete_id) => {
     therapist: therapistResult.rows[0], // { authorization_id, first_name, email }
     athlete: athleteResult.rows[0], // { authorization_id, first_name, email }
   };
-
-}
+};
 
 const getTherapistStripeAccountId = async (therapist_id) => {
   const result = await pool.query(
@@ -89,7 +88,7 @@ const getTherapistStripeAccountId = async (therapist_id) => {
     [therapist_id]
   );
   return result.rows[0].stripe_account_id;
-}
+};
 
 // cron job to run at midnight and send reminder emails to all therapists and athletes with appointments the next day
 schedule.scheduleJob("35 * * * *", async () => {
@@ -105,23 +104,35 @@ schedule.scheduleJob("35 * * * *", async () => {
   const bookings = bookingQueryResult.rows;
   bookings.forEach(async (booking) => {
     try {
-      const { therapist, athlete } = await getAthleteTherapistContactInfo(booking.fk_therapist_id, booking.fk_athlete_id);
+      const { therapist, athlete } = await getAthleteTherapistContactInfo(
+        booking.fk_therapist_id,
+        booking.fk_athlete_id
+      );
       if (!sentEmails.has(therapist.email)) {
-        emailService.sendBookingReminderEmail(therapist.email, therapist.first_name);
+        emailService.sendBookingReminderEmail(
+          therapist.email,
+          therapist.first_name
+        );
         sentEmails.add(therapist.email);
       }
       if (!sentEmails.has(athlete.email)) {
-        emailService.sendBookingReminderEmail(athlete.email, athlete.first_name);
+        emailService.sendBookingReminderEmail(
+          athlete.email,
+          athlete.first_name
+        );
         sentEmails.add(athlete.email);
       }
     } catch (err) {
-      console.error(`Error sending booking reminder email for booking: bookingId: ${booking.bookings_id}`, err);
+      console.error(
+        `Error sending booking reminder email for booking: bookingId: ${booking.bookings_id}`,
+        err
+      );
     }
   });
 });
 
 // TODO: schedule job to run 30 minutes after midnight to charge athletes for their appointments
-schedule.scheduleJob("35 * * * *", async () => {
+schedule.scheduleJob("40 * * * *", async () => {
   // charge payment intent
   try {
     console.warn("charging payment intents");
@@ -132,28 +143,34 @@ schedule.scheduleJob("35 * * * *", async () => {
         const bookingId = booking.bookings_id;
         const paymentIntentId = booking.payment_intent_id;
         const therapistId = booking.fk_therapist_id;
-        const therapistStripeAccountId = await getTherapistStripeAccountId(therapistId);
+        const therapistStripeAccountId = await getTherapistStripeAccountId(
+          therapistId
+        );
         console.warn("therapistStripeAccountId = ", therapistStripeAccountId);
-        const paymentIntentCapture = await stripe.paymentIntents.capture(paymentIntentId, {
-          amount_to_capture: booking.total_cost,
-          transfer_data: {
-            destination: therapistStripeAccountId,
-          },
-        });
+        const paymentIntentCapture = await stripe.paymentIntents.capture(
+          paymentIntentId,
+          {},
+          {
+            stripeAccount: therapistStripeAccountId,
+          }
+        );
         console.warn("paymentIntentCapture = ", paymentIntentCapture);
-        await  updateBookingStatus(bookingId, "Paid");
-        console.warn(`Payment for booking ID ${bookingId} successful. (Payment Intent: ${paymentIntentCapture})`);
+        await updateBookingStatus(bookingId, "Paid");
+        console.warn(
+          `Payment for booking ID ${bookingId} successful. (Payment Intent: ${paymentIntentCapture})`
+        );
       } catch (error) {
-        console.error(`Error capturing payment for booking ID ${booking.bookings_id}:`, error);
+        console.error(
+          `Error capturing payment for booking ID ${booking.bookings_id}:`,
+          error
+        );
         await updateBookingStatus(booking.bookings_id, "CancelledRefunded");
       }
     });
   } catch (error) {
     console.error("Error batch charging payment intents:", error);
   }
-
 });
-
 
 const port = process.env.PORT || config.get("port");
 
